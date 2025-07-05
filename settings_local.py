212ff8f8c31ad4dac84126d6647b061c02ad85fc
jochenGCD <gcdmail.garcke.de>
2025-07-05 13:39:09 +0200

    update check

--- settings_local.py
+++ settings_local.py
@@ -12,7 +12,7 @@ DATABASES = {
     }
 }
 
-SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error', 'models.E025',
+SILENCED_SYSTEM_CHECKS = ['django_recaptcha.recaptcha_test_key_error', 'models.E025',
                           'fields.W903']
 
 ALLOWED_HOSTS = [
